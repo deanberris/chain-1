e5ed11c67b7a852769dbbd46a3c36eafbcc4c275
Dean Michael Berris <mikhailberis@gmail.com>
2011-03-17 21:35:01 +0800

    Initial interface test and implementation.

---
+++ boost/chain/chain.hpp
@@ -0,0 +1,58 @@
+// Copyright 2011 Dean Michael Berris.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+#ifndef BOOST_CHAIN_CHAIN_HPP_20110317
+#define BOOST_CHAIN_CHAIN_HPP_20110317
+
+#include <memory> // for std::allocator
+#include <string> // for std::string
+
+namespace boost { namespace chains {
+
+    /**
+     * Definition of the basic chain interface.
+     */
+    template <class CharT>
+    struct basic_chain {
+
+        /// Default constructor
+        basic_chain() {}
+
+        /// Construct from CharT const *
+        basic_chain(CharT const *) {}
+
+        /// Construct from std::string
+        template <class StrAllocator>
+        basic_chain(std::basic_string<CharT, StrAllocator> const &) {}
+
+    };
+
+    typedef basic_chain<char> chain;
+
+    template <class CharT, class Allocator = std::allocator<CharT> >
+    struct basic_builder {
+
+        basic_chain<CharT> const chain() {
+            // FIXME implement the builder on demand here
+            return basic_chain<CharT>();
+        }
+
+    };
+
+    typedef basic_builder<char> builder;
+
+    template <class CharT, class Allocator, class T>
+    inline basic_builder<CharT, Allocator> const &
+    operator<<(basic_builder<CharT, Allocator> const & builder_, T const & data) {
+        // FIXME implement the append extension point
+        // append(builder_, data); // find via ADL
+        return builder_;
+    }
+    
+} /* chains */
+
+} /* boost */
+
+#endif /* BOOST_CHAIN_CHAIN_HPP_20110317 */
---
+++ libs/chain/test/interface.cpp
@@ -0,0 +1,37 @@
+// Copyright 2011 Dean Michael Berris.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+/** This test defines the minimal interface requirement for the
+ *  chain data type.
+ */
+
+#include <boost/chain/chain.hpp>
+#include <string>
+
+namespace chains = boost::chains;
+
+int main(int argc, const char *argv[]) {
+    // A chain is default constructible
+    chains::chain a;
+
+    // This is the defacto way for building a chain
+    // from literals and other data types. This should
+    // be familiar because it looks pretty much like how
+    // std::ostringstream works.
+    chains::builder b;
+    b << "Hello, world!";
+
+    // We allow for assignment, in which case a chain is
+    // really a proxy.
+    chains::chain c = b.chain();
+
+    // We allow for constructing a chain from a char const *
+    chains::chain d = "Construction from char const * supported!";
+
+    // We allow for constructing a chain from a std::string
+    chains::chain e = std::string("Construct from a std::string!");
+
+    return 0;
+}
